332613427e5e713cf90b6d068f3c9504ac0db101
carol-solivos <carol.solivos@gmail.com>
2018-03-15 21:34:30 -0400

    api renderizada

--- src/components/Home/Images/index.js
+++ src/components/Home/Images/index.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
+
 class Images extends Component {
 
-  render () {
-    return (
-      <img src="https://facebook.github.io/react-native/docs/assets/favicon.png" />
-    )
+  constructor(props) {
+    super(props)
+    this.state = { data: [] }
   }
-}
 
+  componentWillMount() {
+    fetch('https://api.nasa.gov/planetary/apod?date=2012-12-12&api_key=Tqbm56QokO6KA0u6wjbcnSVeiYwlsq9dNK46y7fE')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {        
+        this.setState({ data: data })
+      })
+  }
+
+  render() {
+    console.log(this.state.data);
+    const title = this.state.data.title;
+    const image = this.state.data.url;
+    if (Object.keys(this.state.data).length > 0) {
+
+      return (
+        <div className="container-fluid">
+          {title}
+          <img src={image}/>
+        </div>
+      )
+    } else {
+      return <p className="text-center">Cargando imagen...</p>
+    }
+  }
+
+}
 
 export default Images;
